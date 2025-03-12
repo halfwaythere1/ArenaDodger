64b5d639b4693f2b511bf74c91f58cc61f67cdde
Halfway There <67455065+halfwaythere1@users.noreply.github.com>
2025-03-12 06:17:19 -0400

    Add files via upload

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,1 +1,16 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("ArenaDodger", "enUS", true)
 
+if L then
+    L["ArenaDodger"] = "ArenaDodger"
+    L["Add Player"] = "Add Player"
+    L["Enter a player name:"] = "Enter a player name:"
+    L["Refresh"] = "Refresh"
+    L["Not In Arena"] = "Not In Arena"
+    L["Nagrand Arena"] = "Nagrand Arena"
+    L["Ruins of Lordaeron"] = "Ruins of Lordaeron"
+    L["Blade's Edge Arena"] = "Blade's Edge Arena"
+    L["Dalaran Arena"] = "Dalaran Arena"
+    L["Ring of Valor"] = "Ring of Valor"
+    L["Remove"] = "Remove"
+    L["Unknown"] = "Unknown"
+end
---
+++ Locales/esMX.lua
@@ -0,0 +1,16 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("ArenaDodger", "esMX", true)
+
+if L then
+    L["ArenaDodger"] = "ArenaDodger"
+    L["Add Player"] = "Añadir Jugador"
+    L["Enter a player name:"] = "Introduce nombre del jugador:"
+    L["Refresh"] = "Actualizar"
+    L["Not In Arena"] = "No está en arena"
+    L["Nagrand Arena"] = "Arena de Nagrand"
+    L["Ruins of Lordaeron"] = "Ruinas de Lordaeron"
+    L["Blade's Edge Arena"] = "Blade's Edge Arena"
+    L["Dalaran Arena"] = "Arena de Dalaran"
+    L["Ring of Valor"] = "Círculo del Valor"
+    L["Remove"] = "Remove"
+    L["Unknown"] = "Unknown"
+end
